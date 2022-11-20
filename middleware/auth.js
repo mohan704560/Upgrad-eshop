e0b9e4bcaf64e4cb67cb399ccde9f253b004a8bd
Mohan Prajapati <mohanprajapati0203@outlook.com>
2022-11-20 11:36:13 +0530

    Auth error updated

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,12 +2,12 @@ const jwt = require("jsonwebtoken");
 
 const auth=(req,res,next)=>{
    try{
-    const token=req.cookies.eshop;
-    const varifyUser = jwt.verify(token,"987654321123456789");
+    const token=req.cookies.ehop;
+    const varifyUser = jwt.verify(token.Token,"987654321123456789");
     next();
    }
    catch(error){
-    res.status(401).send(error);
+    res.status(401).send({success:false,Response:"Please Login first to access this endpoint!"});
    }
 }
 
